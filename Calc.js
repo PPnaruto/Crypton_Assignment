719d2f2ba1ecef60fcd06faa6821f77b9fc9d80b
PPnaruto <prtaik.35.patil@gmail.com>
2023-04-04 13:01:28 +0530

    Program completed, taking input from terminal and calculating as per the input

--- Calc.js
+++ Calc.js
@@ -1,30 +1,34 @@
 
-
-const que = process.argv[2];
-
-const arr = que.split(" ");
-
-console.log(que);
-
-console.log(arr);
-
 let program = (value1, value2, process) => {
+     
     switch (process){
         case "plus":
             return value1 + value2;
         case "minus":
             return value1 - value2;
         case "multiplied":
-            return value1*value2;
+            return value1 * value2;
         case "divided":
-            return value1/value2;
+            return value1 / value2;
         default:
             return "Check Input";
     }
        
 }
 
-const ans = program(5,2,"plus");
+const que = process.argv[2];
 
-console.log(ans);
+const arr = que.split(" ");
+
+// console.log(arr);
+
+const len = arr.length;
+
+const ans = program(+arr[2],+arr[len-2],arr[3]);
+
+if(arr[3] == "multiplied" || arr[3] == "divided"){
+    arr[3] = arr[3]+" "+"by";
+}
+
+console.log(arr[2]+" "+arr[3]+" "+arr[len-2]+" "+"is"+" "+ans);
 
